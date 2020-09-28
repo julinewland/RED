78867007f57eb63cec284d987c2b1c67990cb479
julinewland <jnewland@udesa.edu.ar>
2020-09-28 08:58:02 -0300

    Post

--- red-social/routes/post.js
+++ red-social/routes/post.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var router = express.Router();
-let postController = require("../controllers/postController")
+let postController = require("../controllers/postController");
+const { route } = require("./registracion");
 
 router.get("/detalle/:id", postController.detalle);
+
+router.get("/agregar", postController.agregar)
 
 module.exports = router;
